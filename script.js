98d9d0d03fa7aceb3f23c1fbd758138220d4668b
Bennie <fub8339@uwec.edu>
2020-03-13 12:07:31 -0500

    changes0313

---
+++ script.js
@@ -0,0 +1,14 @@
+
+      const menuButton=document.getElementById('menu-button');
+      const navigation=document.getElementById('navigation');
+
+      menuButton.addEventListener('click',event =>{
+          menuButton.classList.toggle('active');
+          navigation.classList.toggle('open');
+          event.stopPropagation();
+      });
+      document.addEventListener('click',() =>{
+          menuButton.classList.remove('active');
+          navigation.classList.remove('open');
+      });
+     
